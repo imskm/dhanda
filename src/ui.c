ea6644a97e90906aaa5ca25a749a7d99b9d16b2b
Safwan Haider <safwanhaider1508@gmail.com>
2022-02-22 19:33:44 +0530

    Done all ui functions

--- src/ui.c
+++ src/ui.c
@@ -4,6 +4,7 @@
 void
 ui_home(dhanda *app)
 {
+	ui_txn_list(app);
 	int txn = 567,
 		amount = 12399,
 		party = 56,
@@ -88,6 +89,21 @@ void ui_party_edit(struct dhanda *app)
 
 void ui_txn_list(struct dhanda *app)
 {
+	char ptype1[32] = "CREDITED", ptype2[32] = "DEBITED";
+	char name1[64] = "Safwan", name2[64] = "Habibi", name3[64] = "Iqra", name4[64] = "Hero";
+	int id1 = 4, id2 = 3, id3 = 2, id4 = 1;
+	int amount1 = 31231, amount2 = 34334, amount3 = 33324, amount4 = 4545;
+	printf("TRANSACTION ----- HISTORY\n\n");
+
+	printf("ID         NAME            AMOUNT          PTYPE\n");
+	printf("__         ____            ______          _____");
+	printf("\n\n");
+	printf("%-10d %-15s %-15d %-15s\n\n", id1, name1, amount1, ptype2);
+	printf("%-10d %-15s %-15d %-15s\n\n", id2, name2, amount2, ptype1);
+	printf("%-10d %-15s %-15d %-15s\n\n", id3, name3, amount3, ptype2);
+	printf("%-10d %-15s %-15d %-15s\n\n", id4, name4, amount4, ptype1);
+
+
 }
 
 
@@ -122,4 +138,11 @@ void ui_txn_create(struct dhanda *app)
 
 void ui_party_delete(struct dhanda *app)
 {
+	char name[64] = "Safwan";
+	long int phone = 9748538411;
+
+	printf("PARTY DELETE\n\n");
+	printf("NAME  : %s", name);
+	printf("PHONE : %ld", phone);
+
 }
