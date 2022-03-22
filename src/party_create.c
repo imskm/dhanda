6f7456c8e3097a3a7d236790eeca975d69b6d231
Tanweer Kamal <tanweerkamal54@gmail.com>
2022-03-22 20:45:53 +0530

    Party_create function done

--- src/party_create.c
+++ src/party_create.c
@@ -3,11 +3,25 @@
 
 int party_add(dhanda *app, party *party)
 {
+      struct party p;
+      int new_id;
+      
+
       int cur_pos = 0 , final_pos = 0;
 
       debug_print("");
 
       fseek(app->party_fp, 0, SEEK_END);
+      if(!ftell(app->party_fp)) {
+           new_id = 1; 
+      }else{
+           fseek(app->party_fp, -sizeof(*party), SEEK_END);
+           fread(&p, sizeof(p), 1, app->party_fp);
+           new_id = p.id;
+           new_id++; 
+      }
+      party->id = new_id;
+
       cur_pos = ftell(app->party_fp);
       fwrite(party, sizeof(*party), 1, app->party_fp);
       final_pos = ftell(app->party_fp);
